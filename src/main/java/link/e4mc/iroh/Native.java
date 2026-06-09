b0f39d6c71642a498f5094b794c19b001217c13d
FifthTundraG <117035030+FifthTundraG@users.noreply.github.com>
2026-06-09 17:17:24 -0400

    Use $XDG_CACHE_HOME on Linux

--- src/main/java/link/e4mc/iroh/Native.java
+++ src/main/java/link/e4mc/iroh/Native.java
@@ -14,6 +14,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.security.MessageDigest;
 import java.util.Arrays;
+import java.util.Objects;
 
 public class Native {
     static {
@@ -30,11 +31,26 @@ public class Native {
         boolean downloaded = false;
 
         while (libraryPath == null) {
-            String home = System.getProperty("user.home");
-            String fileName = System.mapLibraryName(libName);
-            String folderPath = home + File.separatorChar + ".e4mc_cache";
-            libraryPath = folderPath + File.separatorChar + fileName;
+            String fileName;
+            String folderPath;
+            if (Objects.equals(PlatformDependent.normalizedOs(), "linux")) { // use XDG_CACHE_HOME on Linux, see https://github.com/vgskye/e4mc-minecraft-architectury/issues/243
+                String xdgCacheHome = System.getenv("XDG_CACHE_HOME");
+                if (xdgCacheHome == null) {
+                    xdgCacheHome = Paths.get(System.getProperty("user.home"), ".cache").toString();
+                }
+
+                fileName = System.mapLibraryName(libName);
+                folderPath = Paths.get(xdgCacheHome, "e4mc_cache").toString();
+                libraryPath = Paths.get(folderPath, fileName).toString();
+            } else {
+                String home = System.getProperty("user.home");
+                fileName = System.mapLibraryName(libName);
+                folderPath = home + File.separatorChar + ".e4mc_cache";
+                libraryPath = folderPath + File.separatorChar + fileName;
+            }
+
             new File(folderPath).mkdirs();
+
             if (!new File(libraryPath).isFile()) {
                 try {
                     URL url = new URL(System.getProperty("link.e4mc.dialtone.native_url", "https://natives.e4mc.link/" + fileName));
